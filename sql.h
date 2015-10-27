d11e2b1c14f4837ccfb17358f92ba1904f8a7160
samcia1988 <ranger.ying@gmail.com>
2015-10-27 17:46:00 +0800

    Create time and finish time within.

--- sql.h
+++ sql.h
@@ -1,13 +1,13 @@
 #define CREATE_AREA_TABLE_SQL "create table area(id integer primary key autoincrement,name varchar(128));"
-#define CREATE_TASK_TABLE_SQL "create table task(id integer primary key autoincrement,title varchar(128),content varchar(1024),state varchar(2),areaId integer);"
+#define CREATE_TASK_TABLE_SQL "create table task(id integer primary key autoincrement,title varchar(128),content varchar(1024),state varchar(2),areaId integer,createTime datetime,finishTime datetime);"
 #define CREATE_CONFIG_TABLE_SQL "create table config(id integer primary key autoincrement,name varchar(128),attr varchar(128));"
 
 #define INSERT_CURRENT_AREA_CONFIG_SQL "insert into config (name,attr) values ('current_area',NULL);"
 #define INSERT_AREA_SQL "insert into area (name) values ('%s');"
-#define INSERT_TASK_SQL "insert into task (title,content,state,areaId) values ('%s','%s','%s','%ld');"
+#define INSERT_TASK_SQL "insert into task (title,content,state,areaId,createTime) values ('%s','%s','%s','%ld',datetime('now'));"
 
 #define UPDATE_SWITHCED_AREA_SQL "update config set attr='%s' where name='current_area';"
-#define UPDATE_TASK_STATE_SQL "update task set state='%s' where  id='%ld';"
+#define UPDATE_TASK_STATE_SQL "update task set state='%s',finishTime=datetime('now') where  id='%ld';"
 
 #define QUERY_ALL_AREA_SQL "select name from area;"
 #define QUERY_CURRENT_AREA_SQL "select area.name from config left join area on config.attr=area.id where config.name='current_area';"
